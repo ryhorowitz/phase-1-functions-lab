6574fb25e15015f191a4b20e4fdec214ec69a429
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-12-16 12:15:54 -0500

    commit tree f2d4622e537bb951e7c0e658d64b6ba4fd7b078f

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(value) {
+
+  return Math.abs(42 - value)
+}
+
+function distanceFromHqInFeet(value) {
+  return distanceFromHqInBlocks(value) * 264
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+  const blocksTravelled = Math.abs(start - destination)
+  return blocksTravelled * 264
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination)
+  
+  if (distance < 401) {
+    return 0
+  } else if (distance > 2500) {
+    return 'cannot travel that far'
+  } else if (distance > 2000 && distance < 2500) {
+    return 25
+  } else {
+    return (distance - 400) * 2 / 100
+  }
+}
+
+
